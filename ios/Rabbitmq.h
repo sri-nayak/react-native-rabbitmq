e66169d196e839599118a41d473a6dcda5650e20
bathala.teju@prodevans.com <sridhara.nayak@prodevans.com>
2024-06-28 19:13:31 +0530

    first library

--- ios/Rabbitmq.h
+++ ios/Rabbitmq.h
@@ -5,6 +5,8 @@
 @interface Rabbitmq : NSObject <NativeRabbitmqSpec>
 #else
 #import <React/RCTBridgeModule.h>
+#import <RMQClient/RMQClient.h>
+#import <RMQClient/RMQChannel.h>
 
 @interface Rabbitmq : NSObject <RCTBridgeModule>
 #endif
